4f6560cd37584065aaa131363d63afbc6734d68d
voquocminh1209 <voquocminh1209@gmail.com>
2023-02-12 23:24:10 +0700

    fix: Lazy loading newfeeds

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -4,7 +4,6 @@ import './HomePage.scss';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../app/state.type';
 import Header from '../../components/Header/Header';
-import HomeSkeleton from '../../components/SkeletonLoading/HomeSkeleton';
 import AllLikesPopup from '../../components/AllLikesPopup/AllLikesPopup';
 import PostComment from '../../components/PostComment/PostComment';
 import { useHomePage } from './hooks/useHomePage';
@@ -16,17 +15,18 @@ import { addNewComment } from './state/homeActions';
 import LazyLoad from 'react-lazyload';
 import PostSkeleton from '../../components/SkeletonLoading/PostSkeleton';
 import { useAppDispatch } from '../../app/store';
+import InfinitePostList from './components/InfinitePostList/InfinitePostList';
 
 const HomePage = (): ReactElement => {
+
   useHomePage();
-  const { listPost, listComment, isLoading, loadListPostFail } = useSelector((state: AppState) => {
+
+  const {listComment, loadListPostFail } = useSelector((state: AppState) => {
     return state.home;
   });
-  const { showDetail, handleLikePost } = usePostItem();
   const { isShowPostDetail, selectedPost } = useSelector((state: AppState) => state.home);
   const { hideDetail, handleLikePostComment } = usePostComment();
 
-  console.log("listpost:", listPost)
   return (
     <>
       <Container fluid>
@@ -41,29 +41,12 @@ const HomePage = (): ReactElement => {
           </Row>
         ) : (
           <Row>
-            {(isLoading as boolean) ? (
-              <HomeSkeleton />
-            ) : (
-              <>
-                <Col md={{ span: 7 }}>
-                  {listPost.map((post: any, index: number) => {
-                    return (
-                      <LazyLoad key={index} placeholder={<PostSkeleton></PostSkeleton>}>
-                        <PostItem
-                          key={index}
-                          post={post}
-                          handleLikePost={handleLikePost}
-                          showDetail={showDetail}
-                        />
-                      </LazyLoad>
-                    );
-                  })}
-                </Col>
-                <Col md={{ span: 4, offset: 1 }}>
-                  <Category />
-                </Col>
-              </>
-            )}
+            <Col md={{ span: 7 }}>
+              <InfinitePostList/>
+            </Col>
+            <Col md={{ span: 4, offset: 1 }}>
+              <Category />
+            </Col>
           </Row>
         )}
       </Container>
--- src/pages/Home/hooks/useHomePage.ts
+++ src/pages/Home/hooks/useHomePage.ts
@@ -16,46 +16,4 @@ export const useHomePage = (): any => {
   useEffect(() => {
     void loadRecommend();
   }, []);
-
-  // fix lazing loading
-  const observer = useRef<IntersectionObserver | null>(null);
-  let page = 1;
-  const homePostsParams: GetHomePostsDto = {
-    userId: currentUser.id,
-    pageNumber: page
-  };
-
-  useEffect(()=>{
-    // dispatch(getHomePosts(homePostsParams)).unwrap()
-
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 1.0,
-    };
-
-    observer.current = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          page += 1;
-          const homePostsParams: GetHomePostsDto = {
-            userId: currentUser.id,
-            pageNumber: page
-          };
-          dispatch(getHomePosts(homePostsParams)).unwrap()
-        }
-      });
-    }, options);
-
-    const observerTarget = document.querySelector('#observer-target');
-    if (observerTarget) {
-      observer.current.observe(observerTarget);
-    }
-
-    return () => {
-      if (observer.current) {
-        observer.current.disconnect();
-      }
-    };
-  }, [dispatch])
 };
